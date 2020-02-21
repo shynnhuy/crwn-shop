018c9e6e339ea8af37bb146b3ea7b10af370f0b5
Shynn Huy <shynn@hotmail.com>
2020-02-22 01:15:38 +0700

    Semi finish

--- src/pages/check-out/check-out.component.jsx
+++ src/pages/check-out/check-out.component.jsx
@@ -9,6 +9,7 @@ import {
     selectCartTotal
 } from '../../redux/cart/cart.selectors';
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
 
 const CheckOutPage = ({ cartItems, total }) => {
     return (
@@ -36,6 +37,16 @@ const CheckOutPage = ({ cartItems, total }) => {
             <div className="total">
                 <span>TOTAL: ${total}</span>
             </div>
+            <div className="test-warning">
+                *Please use the following test credit card for payments*
+                <br />
+                4242 4242 4242 4242 - Exp: Any future date - CVC: 123
+                <br />
+                5555 5555 5555 4444 - Exp: Any future date - CVC: 123
+                <br />
+                2223 0031 2200 3222 - Exp: Any future date - CVC: 123
+            </div>
+            <StripeCheckoutButton price={total} />
         </div>
     );
 };
--- src/redux/store.js
+++ src/redux/store.js
@@ -6,7 +6,11 @@ import rootReducer from './root-reducer';
 
 import { persistStore } from 'redux-persist';
 
-const middleWares = [logger];
+const middleWares = [];
+
+if (process.env.NODE_ENV === 'development') {
+    middleWares.push(logger);
+}
 
 const store = createStore(rootReducer, applyMiddleware(...middleWares));
 
